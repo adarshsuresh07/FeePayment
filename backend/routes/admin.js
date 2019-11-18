a49cd66c9f50c5e16b111095483a444337a746fd
adarsh-av13 <dra4474@gmail.com>
2019-11-18 21:49:16 +0530

    setup search backend

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -23,32 +23,51 @@ router.get('/',cors.corsWithOptions, pass.verifyUser, function(req, res, next) {
   });
 });
 
-router.options('/:studentId', cors.corsWithOptions, (req,res) => {res.sendStatus(200); })
-router.get('/:studentId',cors.corsWithOptions, pass.verifyUser, function(req, res, next) {
-  const query = "SELECT s.admno,s.name,s.sem,s.dept,s.paidOrNot,c.scholname,f.deadline,DATE_FORMAT(f.deadline,'%d %M %Y') as dlday FROM students s,fees f,scholarships c WHERE s.sem=f.sem and s.scholId=c.scholId and s.admno="+req.params.studentId;
-  db.query(query,function(err,result){
-    let fine = 'No';
-    let paid = result[0].paidOrNot === 0?'No':'Yes';
-    let today = new Date();
-    let dayslate = Math.floor((today.getTime() - result[0].deadline.getTime())/(1000*60*60*24));
-    if(dayslate>0) {
-      fine = 'Yes';
+router.options('/search', cors.corsWithOptions, (req,res) => {res.sendStatus(200); })
+router.get('/search',cors.corsWithOptions, pass.verifyUser, function(req, res, next) {
+  let query = "SELECT s.admno,s.name,s.sem,s.dept,s.paidOrNot,c.scholname,f.deadline,DATE_FORMAT(f.deadline,'%d %M %Y') as dlday FROM students s,fees f,scholarships c WHERE s.sem=f.sem and s.scholId=c.scholId";
+  let keys = Object.keys(req.body);
+  let values = Object.values(req.body);
+  for(let i=0;i<keys.length;++i) {
+    let table = 's';
+    if(keys[i] == 'scholname') {
+      table = 'c';
     }
-    let dept = depts[result[0].dept];
-    res.statusCode = 200;
-    let row = Object.assign(
-      {
-        'admno': result[0].admno,
-        'name': result[0].name,
-        'sem': result[0].sem,
-        'schol': result[0].scholname,
-        'deadline': result[0].dlday,
-        'dept': dept, 
-        'fine': fine, 
-        'paid': paid
+    query+=(" and "+table+"."+keys[i]+"='"+values[i]+"'");
+  }
+  db.query(query,function(err,result){
+    if(err)
+      return res.json({error: true, msg: 'Something went wrong'});
+    if(result.length == 0) {
+      return res.json({error: true, msg: 'No students found'});
+    }
+    let rows = [];
+    result.forEach((row) => {
+      let fine = 'No';
+      let paid = row.paidOrNot === 0?'No':'Yes';
+      let today = new Date();
+      let dayslate = Math.floor((today.getTime() - row.deadline.getTime())/(1000*60*60*24));
+      if(row.paidOrNot && dayslate>0) {
+        fine = 'Yes';
       }
-    );
-    res.json(row);
+      let dept = depts[row.dept];
+      res.statusCode = 200;
+      let resRow = Object.assign(
+        {
+          'admno': row.admno,
+          'name': row.name,
+          'sem': row.sem,
+          'schol': row.scholname,
+          'deadline': row.dlday,
+          'dept': dept, 
+          'fine': fine, 
+          'paid': paid,
+          'error': false
+        }
+      );
+      rows.push(resRow);
+    });
+    res.json(rows);
   });
 });
 
