1c11ded8bbb8f9329750c1228b4c3b69d16cdb51
adarshsuresh07 <adarshsuresh@cet.ac.in>
2019-11-21 00:22:57 +0530

    table temp

--- src/Components/Adminhome.js
+++ src/Components/Adminhome.js
@@ -16,7 +16,13 @@ class Adminhome extends React.Component  {
       sem: '',
       dept: '',
       paidornot: '',
-      scholname: ''
+      scholname: '',
+      students: [
+            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
+            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
+            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
+         ]
     };
   };
 
@@ -34,6 +40,25 @@ class Adminhome extends React.Component  {
       });
     });
   };
+  renderTableHeader() {
+      let header = Object.keys(this.state.students[0])
+      return header.map((key, index) => {
+         return <th key={index}>{key.toUpperCase()}</th>
+      })
+   }
+   renderTableData() {
+      return this.state.students.map((student, index) => {
+         const { id, name, age, email } = student //destructuring
+         return (
+            <tr key={id}>
+               <td>{id}</td>
+               <td>{name}</td>
+               <td>{age}</td>
+               <td>{email}</td>
+            </tr>
+         )
+      })
+   }
 
     handleLogout(){
         logouta();
@@ -139,8 +164,14 @@ class Adminhome extends React.Component  {
      </span >
   </div> 
 </div>
-<div className={Style.student_table}>
- Space for Table
+<div className={Style.studenttable}>
+ <h1 id='title'>React Dynamic Table</h1>
+            <table id='students'>
+               <tbody>
+                  <tr>{this.renderTableHeader()}</tr>
+                  {this.renderTableData()}
+               </tbody>
+            </table>
 </div>
 </div>
 
