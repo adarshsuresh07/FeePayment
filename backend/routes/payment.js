a3f9136e33c98376ee7263ec2b20971bbe6300cd
adarsh-av13 <dra4474@gmail.com>
2019-11-26 01:18:18 +0530

    date converted to ist

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -5,27 +5,15 @@ var pass = require('../passport-config');
 var db = require('../mysql-config');
 const paymentRouter = express.Router();
 
-// const stripeChargeCallback = res => (stripeErr,stripeRes) => {
-//   if(stripeErr)
-//     res.status(500).send({error: StripeErr});
-//   else
-//     res.status(200).send({success: stripeRes});  
-// };
-
-// function curDateInIST() {
-//   let date = new Date();
-//   console.log(date);
-//   let localTime = date.getTime();
-//   let localOffset = date.getTimezoneOffset()*60000;
-//   console.log(localOffset);
-//   let utc = localTime + localOffset;
-//   let utcdate = new Date(utc);
-//   console.log(utcdate);
-//   let offset = 5.5;
-//   let ist = utc+(3600000*offset);
-//   let istDate = new Date(ist);
-//   return istDate;
-// }
+function getISTDate() {
+  let dateUTC = new Date();
+  let dateUTC = dateUTC.getTime() 
+  let dateIST = new Date(dateUTC);
+  //date shifting for IST timezone (+5 hours and 30 minutes)
+  dateIST.setHours(dateIST.getHours() + 5); 
+  dateIST.setMinutes(dateIST.getMinutes() + 30);
+  return dateIST;
+}
 
 paymentRouter.options('*',cors.corsWithOptions, (req,res) => {res.sendStatus(200); })
 
@@ -37,7 +25,7 @@ paymentRouter.post('/', cors.corsWithOptions, pass.verifyUser, (req,res) => {
   };
   stripe.charges.create(body)
   .then(charge => {
-    let date = new Date();
+    let date = getISTDate();
     date = date.toISOString().slice(0, 19).replace('T', ' ');
     let query = "UPDATE students SET paidornot=1, dateofpayment='"+date+"' WHERE admno='"+req.user.username+"'";
     db.query(query, (err,result) => {
