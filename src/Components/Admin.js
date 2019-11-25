2c53839b5b08600e997a4d7eef2d8a20c16b07af
Ajay Duth <ajayduth123@gmail.com>
2019-11-25 22:23:10 +0530

    Validation student

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -67,6 +67,8 @@ render(){
       <button className={Style.inactive} onClick={this.handleClick}>Student</button>
       <button className={Style.tabbutton}>Admin</button> 
        </div>
+       {this.state.error.length > 0 && (<span className="errorInvalid" > {this.state.error} </span>)}
+
         <form > 
          <input type="text" id="username" name="username" placeholder="CET ID Number" className={Style.input1} value={this.state.username} onChange={this.handleChange} required />
          <input type="password" id="password" name="password" placeholder="Password" className={Style.input1} value={this.state.password} onChange={this.handleChange} required/> 
